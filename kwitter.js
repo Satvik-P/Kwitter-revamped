05b1499216847ccbe3512decfb77ce0a63b929c3
Satvik-P <95187247+Satvik-P@users.noreply.github.com>
2022-06-18 09:41:18 -0700

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,9 +1,8 @@
-function addUser()
-{
-    
+function adduser() {
     user_name = document.getElementById("user_name").value;
-    
-    localStorage.setItem("user_name", user_name);
-    
-    window.location = "Kwitter_room.html";
+
+    localStorage.setItem("Username",user_name);
+
+    window.location = "kwitter_room.html";
 }
+  
